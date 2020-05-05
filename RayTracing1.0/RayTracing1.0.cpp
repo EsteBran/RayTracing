ceb00c25ffdd1ba401b15696bfd797e9b8ac5978
estebran <tawseefhanif@gmail.com>
2020-05-05 17:09:00 -0400

    Implemented a way for multiple objects to be rendered using a list and a hittable type.

--- RayTracing1.0/RayTracing1.0.cpp
+++ RayTracing1.0/RayTracing1.0.cpp
@@ -1,32 +1,21 @@
-#include <iostream>
-#include "ray.h"
+#include "utility.h"
+#include "hittable_list.h"
+#include "sphere.h"
+#include "color.h"
 
-//sees if a specific ray hits the sphere and returns value signifying 
-double hit_sphere(const point3& center, double radius, const ray& r) {
-    vec3 oc = r.origin() - center;
-    auto a = dot(r.direction(), r.direction());
-    auto b = 2.0 * dot(oc, r.direction());
-    auto c = dot(oc, oc) - radius * radius;
-    auto discriminant = b * b - 4 * a * c;
-    
-    if (discriminant < 0) {
-        return -1.0;
-    }
-    else {
-        return (-b - sqrt(discriminant)) / (2.0 * a);
-    }
-}
+#include <iostream>
+
+
 
 //returns the color of each ray once it reaches the camera
-color ray_color(const ray& r) {
-    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
-    if (t > 0.0) {
-        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
-        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+color ray_color(const ray& r, const hittable& world) {
+    hit_record rec;
+    if (world.hit(r, 0, infinity, rec)) {
+        return 0.5 * (rec.normal + color(1, 1, 1));
     }
     
     vec3 unit_direction = unit_vector(r.direction());
-    t = 0.5 * (unit_direction.y() + 1.0);
+    auto t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
@@ -44,18 +33,24 @@ int main()
     vec3 horizontal(4.0, 0.0, 0.0);
     vec3 vertical(0.0, 2.25, 0.0);
     point3 origin(0.0, 0.0, 0.0);
-    point3 lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, 1);
+    point3 lower_left_corner(-2.0, -1.0, -1.0);
+
+    hittable_list world;
+    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
 
     //iterates through each ray and writes the image into ppm format
     for (int j = image_height - 1; j >= 0; j--) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; i++) {
-            auto u = double(i) / image_width;
-            auto v = double(j) / image_height;
+            auto u = double(i) / (image_width-1);
+            auto v = double(j) / (image_height-1);
             ray r(origin, lower_left_corner + u * horizontal + v * vertical);
-            color color = ray_color(r);
-            color.write_color(std::cout);
+            
+            color pixel_color = ray_color(r, world);
+            
+            write_color(std::cout, pixel_color);
 
         }
     }
